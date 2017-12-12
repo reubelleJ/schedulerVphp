14b79be7d5fa451bc0ec536b18c6c4f7e1e4abc6
reubelle <reubelle.quejadasjunio01@utrgv.edu>
2017-12-12 13:50:23 -0600

    Added navbar and links in registration.php

--- login.php
+++ login.php
@@ -39,27 +39,42 @@
     <div class="starter-template">
     <div class="container">
         <!-- Navigation bar -->
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
       <a class="navbar-brand" href="#">Scheduler</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-                  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                    <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="register.php">Register</a>
-                      </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                     </form>
-                  </div>
-                </nav>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prices</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Standard Template</a>
+              <a class="dropdown-item" href="#">Custom Template</a>
+              <a class="dropdown-item" href="#">Web Development</a>
+            </div>
+          </li>
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+        <ul class="nav navbar-nav navbar-right">
+          <li class="nav-item">
+            <a href="login.php"><button type="button" class="btn btn-outline-success my-2 my-sm-0">Sign In</button></a>
+          </li>
+          <li class="nav-item">
+           <a href="registration.php"><button type="button" class="btn btn-success">Sign Up</button></a>
+          </li>
+      </div>
+    </nav>
       </div>
     <div class="jumbotron"> 
       <div class="container">
--- registration.php
+++ registration.php
@@ -27,27 +27,42 @@
     
     <div class="container">
         <!-- Navigation bar -->
-        <div class="row">
-            <div class="col-xs-12 col-md-12">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                  <a class="navbar-brand" href="#">Scheduler</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+      <a class="navbar-brand" href="#">Scheduler</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="register.php">Register</a>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prices</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Standard Template</a>
+              <a class="dropdown-item" href="#">Custom Template</a>
+              <a class="dropdown-item" href="#">Web Development</a>
             </div>
-        </div><!-- /* end navigation */ -->
+          </li>
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+        <ul class="nav navbar-nav navbar-right">
+          <li class="nav-item">
+            <a href="login.php"><button type="button" class="btn btn-outline-success my-2 my-sm-0">Sign In</button></a>
+          </li>
+          <li class="nav-item">
+           <a href="registration.php"><button type="button" class="btn btn-success">Sign Up</button></a>
+          </li>
+      </div>
+    </nav><!-- /* end navigation */ -->
         <!-- Registration form -->
         <form action="registration.php" method="post">
           <!-- Company name -->
